937364e2b2056cf41ca68c49283ae234ac670fa7
17hsteward <45041053+17hsteward@users.noreply.github.com>
2024-03-21 14:03:50 -0400

    setup test file

---
+++ src/tests/PopulationTest.java
@@ -0,0 +1,11 @@
+package tests;
+import static org.junit.Assert.assertEquals;
+import org.junit.*;
+
+public class PopulationTest {
+
+    @Test
+	void testEmptySizeZero () {
+		assertEquals(0, 0);
+	}
+}
