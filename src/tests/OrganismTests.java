47ad7fa2417d768c1dc959c9ba87fda7853f7147
natasa-zupanski <zupansnm@rose-hulman.edu>
2024-03-21 13:53:29 -0400

    fitness Tests

--- src/tests/OrganismTests.java
+++ src/tests/OrganismTests.java
@@ -43,7 +43,10 @@ public class OrganismTests {
 
     public void mutateTest() {
         Organism one = new Organism("1111");
+        one.mutate(1);
 
+        Organism four = new Organism("1111");
+        four.mutate(4);
     }
 
     public void setChromosomeTest() {
@@ -52,6 +55,44 @@ public class OrganismTests {
 
     public void getChromosomeTest() {
 
+    }
+
+    public void fitnessOf1sTest() {
+        Organism one = new Organism("0010000", "Num. of 1s");
+
+        Organism thirteen = new Organism("10100010010000000000001000010001000010010011000000110", "Num. of 1s");
+    }
+
+    public void fitnessConsec1sTest() {
+        Organism one = new Organism("0101010101010101010101010101010", "Consec. num. 1s");
+
+        Organism five = new Organism("010010111011111001010100100001111000001101", "Consec. num. 1s");
+    }
+
+    public void fitnessTargetOrganismtest() {
+        Organism target = new Organism(
+                "1010000000101001110001101110101001000101100101010110010110011001000010100011110101000000010011111110",
+                "Target Organism");
+
+        Organism oneOff = new Organism(
+                "1010000000101001110001101110101001000101100101010110010110011001000010100011110101000000010011111111",
+                "Target Organism");
+
+        Organism thirteenOff = new Organism(
+                "1010001000111000110101101010101101011101001101110110010110011101000010100011110100000000010011111110",
+                "Target Organism");
+    }
+
+    // requires: |first| = |second|
+    private int getNumDiffsForStrings(String first, String second) {
+        int diffs = 0;
+        for (int i = 0; i < first.length(); i++) {
+            if (first.charAt(i) != second.charAt(i)) {
+                diffs++;
+            }
+        }
+
+        return diffs;
     }
 
 }
