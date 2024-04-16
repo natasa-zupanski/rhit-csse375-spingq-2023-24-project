a67d8b70bc227080b50e624d45092f584745b6c8
natasa-zupanski <zupansnm@rose-hulman.edu>
2024-04-16 09:24:11 -0400

    begin selection tests - truncation

--- src/mainApp/SelectionTruncation.java
+++ src/mainApp/SelectionTruncation.java
@@ -11,20 +11,27 @@ public class SelectionTruncation implements SelectionStrategy {
 
         // selects organisms and adds those to the result, evaluating when odd or even
         // lengths and potential indices occur to avoid index out of bounds errors
-        for (int i = 0; i < mutateNum; i++) {
-            if (mutateNum % 2 == 0) {
-                if (i % 2 == 0) {
-                    result[i] = orgs[mutateNum - (i / 2) - 1];
-                } else {
-                    result[i] = orgs[mutateNum - ((i + 1) / 2) - 1];
-                }
-            } else {
-                if (i % 2 == 0) {
-                    result[i] = orgs[mutateNum - ((i + 1) / 2) - 1];
-                } else {
-                    result[i] = orgs[mutateNum - (i / 2) - 1];
-                }
-            }
+        // for (int i = 0; i < mutateNum; i++) {
+        // if (mutateNum % 2 == 0) {
+        // if (i % 2 == 0) {
+        // result[i] = orgs[mutateNum - (i / 2) - 1];
+        // } else {
+        // result[i] = orgs[mutateNum - ((i + 1) / 2) - 1];
+        // }
+        // } else {
+        // if (i % 2 == 0) {
+        // result[i] = orgs[mutateNum - ((i + 1) / 2) - 1];
+        // } else {
+        // result[i] = orgs[mutateNum - (i / 2) - 1];
+        // }
+        // }
+        // }
+        for (int i = 0; i < mutateNum / 2; i++) {
+            result[i] = orgs[mutateNum - i - 1];
+            result[mutateNum / 2 + i] = orgs[mutateNum - i - 1];
+        }
+        if (mutateNum % 2 != 0) {
+            result[mutateNum - 1] = orgs[mutateNum - 1];
         }
 
         return result;
--- src/tests/SelectionTests.java
+++ src/tests/SelectionTests.java
@@ -2,7 +2,16 @@ package tests;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.Arrays;
+
 import org.junit.*;
+
+import mainApp.FitnessType;
+import mainApp.Organism;
+import mainApp.RandomType;
+import mainApp.SelectionStrategy;
+import mainApp.SelectionTruncation;
+import mainApp.SelectionType;
 
 // public enum SelectionType {
 //     TRUNCATION,
@@ -16,7 +25,48 @@ import org.junit.*;
 public class SelectionTests {
 
     @Test
-    public void selectionTestTruncation() {
+    public void selectionTruncationTestOne() {
+        int genSize = 100;
+        // int chromSize = 10;
+        String chromOne = "1111111111";
+        String chromTwo = "0000000000";
+        Organism[] orgs = new Organism[genSize];
+        for (int i = 0; i < genSize / 2; i++) {
+            orgs[i] = new Organism(chromOne, FitnessType.NUMONES, RandomType.FAKE);
+        }
+        for (int j = genSize / 2; j < genSize; j++) {
+            orgs[j] = new Organism(chromTwo, FitnessType.NUMONES, RandomType.FAKE);
+        }
+        Arrays.sort(orgs);
+        SelectionStrategy selection = new SelectionTruncation();
+        assertEquals(SelectionType.TRUNCATION, selection.getSelectionType());
+        Organism[] selected = selection.selectFrom(orgs);
+        for (int k = 0; k < genSize; k++) {
+            assertEquals(10, selected[k].fitness());
+            assertEquals(chromOne, selected[k].getChromosome());
+        }
+    }
 
+    @Test
+    public void selectionTruncationTestTwo() {
+        int genSize = 99;
+        // int chromSize = 10;
+        String chromOne = "1111111111";
+        String chromTwo = "0000000000";
+        Organism[] orgs = new Organism[genSize];
+        for (int i = 0; i < genSize / 2 + 1; i++) {
+            orgs[i] = new Organism(chromOne, FitnessType.NUMONES, RandomType.FAKE);
+        }
+        for (int j = genSize / 2 + 1; j < genSize; j++) {
+            orgs[j] = new Organism(chromTwo, FitnessType.NUMONES, RandomType.FAKE);
+        }
+        Arrays.sort(orgs);
+        SelectionStrategy selection = new SelectionTruncation();
+        assertEquals(SelectionType.TRUNCATION, selection.getSelectionType());
+        Organism[] selected = selection.selectFrom(orgs);
+        for (int k = 0; k < genSize; k++) {
+            assertEquals(10, selected[k].fitness());
+            assertEquals(chromOne, selected[k].getChromosome());
+        }
     }
 }
