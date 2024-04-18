450556502f7ce381318ded3cdf3aff74dcc28cc3
17hsteward <45041053+17hsteward@users.noreply.github.com>
2024-04-17 20:41:27 -0400

    fix view test for view features

--- src/tests/ViewTest.java
+++ src/tests/ViewTest.java
@@ -15,7 +15,7 @@ public class ViewTest {
 	public void testFittestOrganism () {
         FittestOrganismViewer view = new FittestOrganismViewer();
         view.setUpViewer();
-        assertEquals(JFrame.EXIT_ON_CLOSE, view.getFrame().getDefaultCloseOperation());
+        assertEquals(JFrame.DISPOSE_ON_CLOSE, view.getFrame().getDefaultCloseOperation());
         assertTrue(view.getFrame().getLayout() instanceof BorderLayout);
 	}
 
@@ -23,7 +23,7 @@ public class ViewTest {
 	public void testGeneration () {
         GenerationViewer view = new GenerationViewer();
         view.setUpViewer();
-        assertEquals(JFrame.EXIT_ON_CLOSE, view.getFrame().getDefaultCloseOperation());
+        assertEquals(JFrame.DISPOSE_ON_CLOSE, view.getFrame().getDefaultCloseOperation());
         assertTrue(view.getFrame().getLayout() instanceof BorderLayout);
 	}
 
@@ -39,7 +39,7 @@ public class ViewTest {
 	public void testOrganism () {
         OrganismViewer view = new OrganismViewer();
         view.setUpViewer();
-        assertEquals(JFrame.EXIT_ON_CLOSE, view.getFrame().getDefaultCloseOperation());
+        assertEquals(JFrame.DISPOSE_ON_CLOSE, view.getFrame().getDefaultCloseOperation());
         assertTrue(view.getFrame().getLayout() instanceof BorderLayout);
 	}
 }
