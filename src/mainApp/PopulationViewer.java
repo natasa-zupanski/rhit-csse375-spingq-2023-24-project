0c3d4f45fb86091fae5cd2889418fe56cfc8d60e
17hsteward <45041053+17hsteward@users.noreply.github.com>
2024-05-09 00:53:09 -0400

    remove warnings

--- src/mainApp/PopulationViewer.java
+++ src/mainApp/PopulationViewer.java
@@ -3,15 +3,10 @@ package mainApp;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.GridLayout;
-import java.awt.Window;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.ComponentListener;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
-import java.awt.event.WindowStateListener;
 
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
@@ -20,7 +15,6 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
-import javax.swing.JToggleButton;
 import javax.swing.SwingConstants;
 import javax.swing.Timer;
 
