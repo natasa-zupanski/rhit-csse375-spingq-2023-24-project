1ed8c9a5b93bb02fd4c22c9f0cd825d98f8d4513
natasa-zupanski <zupansnm@rose-hulman.edu>
2024-05-09 00:17:53 -0400

    better start page

--- src/mainApp/StartViewer.java
+++ src/mainApp/StartViewer.java
@@ -1,9 +1,15 @@
 package mainApp;
 
 import java.awt.BorderLayout;
+import java.awt.ComponentOrientation;
+import java.awt.Dimension;
+import java.awt.LayoutManager;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 public class StartViewer extends Views {
 
@@ -22,8 +28,23 @@ public class StartViewer extends Views {
 
         JButton startButton = new JButton("Start");
         startButton.addActionListener(new FramePassingActionListener(frame));
+        startButton.setSize(new Dimension(100, 50));
 
-        this.frame.add(startButton, BorderLayout.CENTER);
+        JLabel icon = new JLabel();
+        ImageIcon img = new ImageIcon("Logo.png");
+        System.out.println(img.getImage());
+        icon.setIcon(img);
+        icon.setSize(600, 300);
+
+        JPanel first = new JPanel();
+        first.add(icon);
+        frame.add(first, BorderLayout.NORTH);
+        JPanel second = new JPanel();
+        second.add(startButton);
+        frame.add(second, BorderLayout.CENTER);
+        JPanel third = new JPanel();
+        third.add(new JLabel("created by Allyn Loyd, Hailey Steward, and Natasa Zupanski"));
+        frame.add(third, BorderLayout.SOUTH);
 
     }
 }
